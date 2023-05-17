1aba535697ec8e1a7e55851ff49db7a9f1701d00
icar0S <icarooliveira_23@hotmail.com>
2023-05-16 23:53:30 -0300

    feat: methods get, post, put server

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,22 +1,19 @@
-import { log } from 'console'
 import fastify from 'fastify'
-import { PrismaClient } from '@prisma/client'
-
+import cors from '@fastify/cors'
+import { memoriesRoutes } from './routes/memories'
 
 const app = fastify()
-const prisma = new PrismaClient()
 
-app.get('/users', async () => {
-  const users = await prisma.user.findMany()
-
-  return users
+app.register(cors, {
+  origin: true,
 })
 
+app.register(memoriesRoutes)
 
 app
   .listen({
     port: 3333,
   })
   .then(() => {
-    log('🌎🚀 🌑 🪐 🛸 \n SERVER EXECUTING ON \n http://localhost:3333  💨')
+    console.log('🌎🚀 🌑 🪐 🛸 \n SERVER EXECUTING ON \n http://localhost:3333  💨')
   })
